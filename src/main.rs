e91360308e47c52969e6952be74f594ec114a881
Turner Naef <Turner.Naef@gmail.com>
2024-07-16 20:00:25 -0600

    Threads

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 use std::thread;
 
-fn spawn_client_handler(message: String) {
+fn spawn_client_handler(message: String) -> thread::JoinHandle<()> {
     let handle = thread::spawn(move || {
         println!("Thread started: {}", message);
         thread::sleep(std::time::Duration::from_secs(1));
         println!("Thread finished: {}", message);
     });
 
-    handle.join().unwrap();
+    handle
 }
 
 fn main() {
-    println!("Hello, world!");
-
     // Array of messages
     let messages = ["Message 1", "Message 2", "Message 3"];
+    let mut threads = Vec::new();
 
     // Loop to call spawn_client_handler for each message
     for &message in &messages {
-        spawn_client_handler(message.to_string());
+        let handle = spawn_client_handler(message.to_string());
+        threads.push(handle);
+    }
+
+    // Wait for all threads to finish
+    for handle in threads {
+        handle.join().unwrap();
     }
 }
