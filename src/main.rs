22963695f5e0b116bb170143d6dc168ba9fa5b82
Turner Naef <Turner.Naef@gmail.com>
2024-07-17 22:27:42 -0600

    Chat room setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use log::{debug, warn};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, split};
 use tokio::net::TcpListener;
+use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,8 +10,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     debug!("Starting application");
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel(10);
+
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = split(socket);
@@ -19,30 +25,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut line = String::new();
 
             loop {
-                let _bytes_read = match reader.read_line(&mut line).await {
-                    Ok(bytes_read) if bytes_read == 0 => {
-                        debug!("connection closed");
-                        break;
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        match result {
+                            Ok(bytes_read) if bytes_read == 0 => {
+                                debug!("connection closed");
+                                break;
+                            }
+                            Ok(bytes_read) => {
+                                debug!("read {} bytes", bytes_read);
+                                tx.send((line.clone(), addr)).unwrap();
+                                line.clear();
+                            }
+                            Err(e) => {
+                                warn!("failed to read from socket; err = {:?}", e);
+                                break;
+                            }
+                        }
                     }
-                    Ok(bytes_read) => {
-                        debug!("read {} bytes", bytes_read);
-                        bytes_read
-                    }
-                    Err(e) => {
-                        warn!("failed to read from socket; err = {:?}", e);
-                        break;
-                    }
-                };
 
-                if let Err(e) = writer.write_all(line.as_bytes()).await {
-                    warn!("failed to write to socket; err = {:?}", e);
-                    break;
+                    result = rx.recv() => {
+                        let (msg, recv_addr) = result.unwrap();
+
+                        if addr != recv_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                            // if let Err(e) = writer.write_all(msg.as_bytes()).await {
+                            //     warn!("failed to write to socket; err = {:?}", e);
+                            //     break;
+                            // }
+                        }
+                    }
                 }
-
-                line.clear();
             }
         });
     }
-
-    Ok(())
 }
