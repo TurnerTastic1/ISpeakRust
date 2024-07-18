e628337ae4abe8e42fd4c5b328e3f1d934d132d0
Turner Naef <Turner.Naef@gmail.com>
2024-07-17 21:47:06 -0600

    I was overthinking

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,47 @@
-use std::collections::HashMap;
 use log::{debug, warn};
-use tokio::io::{AsyncReadExt, AsyncWriteExt, split};
-use tokio::net::{TcpListener, TcpStream};
-
-struct Session {
-    id: u64,
-    socket: TcpStream
-}
-
-impl Session {
-    async fn new(id: u64, socket: TcpStream) -> Self {
-        debug!("Created session with id: {}", id);
-        Session { id, socket }
-    }
-
-    async fn run(&mut self) {
-        let (mut reader, mut writer) = split(&mut self.socket);
-
-        let mut buf = vec![0; 1024];
-        loop {
-            let n = match reader.read(&mut buf).await {
-                Ok(n) if n == 0 => return,
-                Ok(n) => {
-                    debug!("read {} bytes", n);
-                    n
-                }
-                Err(e) => {
-                    warn!("failed to read from socket; err = {:?}", e);
-                    return;
-                }
-            };
-
-            if let Err(e) = writer.write_all(&buf[..n]).await {
-                warn!("failed to write to socket; err = {:?}", e);
-                return;
-            }
-        }
-    }
-}
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, split};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 
     debug!("Starting application");
-    let listener =TcpListener::bind("127.0.0.1:8080").await?;
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 
-    let mut _sessions: HashMap<u64, Session> = HashMap::new();
-    let mut next_session_id = 0usize;
-
-    while let Ok((socket, addr)) = listener.accept().await {
-        debug!("Accepted connection from: {}", addr);
-
-        let session = Session::new(next_session_id as u64, socket).await;
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
         tokio::spawn(async move {
-            let mut session = session;
-            session.run().await;
-        });
+            let (reader, mut writer) = split(socket);
 
-        next_session_id += 1;
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let _bytes_read = match reader.read_line(&mut line).await {
+                    Ok(bytes_read) if bytes_read == 0 => {
+                        debug!("connection closed");
+                        break;
+                    }
+                    Ok(bytes_read) => {
+                        debug!("read {} bytes", bytes_read);
+                        bytes_read
+                    }
+                    Err(e) => {
+                        warn!("failed to read from socket; err = {:?}", e);
+                        break;
+                    }
+                };
+
+                if let Err(e) = writer.write_all(line.as_bytes()).await {
+                    warn!("failed to write to socket; err = {:?}", e);
+                    break;
+                }
+
+                line.clear();
+            }
+        });
     }
 
     Ok(())
