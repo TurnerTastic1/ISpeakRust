f8061c07272a2d7910773d4780786ff380137894
Turner Naef <Turner.Naef@gmail.com>
2024-07-23 15:16:51 -0400

    ISpeakRust client CLI?

---
+++ ISpeakRustClient/target/debug/ISpeakRustClient.d
@@ -0,0 +1,1 @@
+/Users/turnernaef/workspace/ISpeakRust/ISpeakRustClient/target/debug/ISpeakRustClient: /Users/turnernaef/workspace/ISpeakRust/ISpeakRustClient/src/main.rs
---
+++ ISpeakRustClient/target/debug/deps/ISpeakRustClient-d1c4cd4991f84c1c.d
@@ -0,0 +1,5 @@
+/Users/turnernaef/workspace/ISpeakRust/ISpeakRustClient/target/debug/deps/ISpeakRustClient-d1c4cd4991f84c1c: src/main.rs
+
+/Users/turnernaef/workspace/ISpeakRust/ISpeakRustClient/target/debug/deps/ISpeakRustClient-d1c4cd4991f84c1c.d: src/main.rs
+
+src/main.rs:
